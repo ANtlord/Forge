cee33a5a70c699a4f257f164a0619d582270b423
antlord <antlord92@gmail.com>
2014-11-21 03:51:24 +0800

    each method has been updated

--- include/each.h
+++ include/each.h
@@ -1,52 +1,60 @@
 #ifndef EACH_H
 #define EACH_H
 
+#include <iostream>
 #include <functional>
 #include <vector>
 
 namespace forge {
-    /**
-        Method run over specified vector, makes specified function, saves
-        result of this function to result vector and returns it.
+    /** \brief Method run over specified vector, makes specified function,
+     * saves result of this function to result vector and returns it.
      */
     template<typename T, typename ResType>
     std::vector<ResType> each(std::function<ResType (const T& value)> func,
         const std::vector<T>& array)
     {
         std::vector<ResType> items;
-        for(uint32_t i=0; i<array.size(); ++i) {
-            items.push_back(func(array.at(i)));
+        for (auto it = array.begin(); it != array.end(); ++it) {
+            items.push_back(func(*it));
         }
         return items;
     }
 
-    /**
-        Method run over specified vector and makes specified function without
-        changing specified vector.
+    /** \brief Method run over specified vector and makes specified function
+     * without changing specified vector.
      */
     template<typename T>
     void each(std::function<void (const T& value)> func,
         const std::vector<T>& array)
     {
-        for(uint32_t i=0; i<array.size(); ++i) {
-            func(array.at(i));
+        for (auto it = array.begin(); it != array.end(); ++it) {
+            func(*it);
         }
     }
 
 
-    /**
-        Method run over specified vector and makes specified function with
-        changing specified vector.
+    /** \brief Method run over specified vector and makes specified function
+     * with changing specified vector.
      */
     template<typename T>
     void each(std::function<void (T& value)> func,
         std::vector<T>& array)
     {
-        for(uint32_t i=0; i<array.size(); ++i) {
-            func(array.at(i));
+        for (auto it = array.begin(); it != array.end(); ++it) {
+            func((*it));
         }
     }
 
+    /** \brief Method run over specified array and makes specified function
+     * with changing specified vector.
+     */
+    template<typename T>
+    void each(std::function<void (T& value)> func, T array[], uint32_t n)
+    {
+        for(uint32_t i=0; i<n; ++i) {
+            func(array[i]);
+        }
+    }
 }
 
 #endif //EACH_H
